6557150bf0f65d75b67f480ba8796aa5eab1788c
Luan Torres <40635192+luantorrex@users.noreply.github.com>
2025-07-30 13:04:16 -0300

    Enable CORS

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .views.note_view import router as note_router
 from .views.user_view import router as user_router
 
 app = FastAPI(title="Zettelkasten API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(note_router, prefix="/notes", tags=["notes"])
 app.include_router(user_router, prefix="/users", tags=["users"])
