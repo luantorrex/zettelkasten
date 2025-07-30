f0330f655254e253b8d836d74ef00ec95737f43a
Luan Torres <40635192+luantorrex@users.noreply.github.com>
2025-07-30 12:18:51 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -10,4 +10,4 @@ app.include_router(user_router, prefix="/users", tags=["users"])
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8080, reload=True)
